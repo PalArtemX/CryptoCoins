d210ca4c5a32142948b1f2b5b20233accc2b2d65
Artem Paliutin <pal.artem@icloud.com>
2022-01-05 21:09:28 +0300

    Initial Commit

---
+++ CryptoCoins/CryptoCoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinsApp.swift
+//  CryptoCoins
+//
+//  Created by Artem Paliutin on 05.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
