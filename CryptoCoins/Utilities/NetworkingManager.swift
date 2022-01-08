128b2a76521af24abd9dbcb9563255d815c5d35c
Artem Paliutin <pal.artem@icloud.com>
2022-01-08 18:52:34 +0300

    added Networking Manager

---
+++ CryptoCoins/Utilities/NetworkingManager.swift
@@ -0,0 +1,53 @@
+//
+//  NetworkingManager.swift
+//  CryptoCoins
+//
+//  Created by Artem Paliutin on 08.01.2022.
+//
+
+import Foundation
+import Combine
+
+
+class NetworkingManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): return "⚠️ Bad response from URL: \(url)"
+            case .unknown: return "⚠️ Unknown error"
+            }
+        }
+    }
+    
+    // MARK: - download
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    // MARK: - handleCompletion
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+    
+    // MARK: - handleURLResponse
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+                  throw NetworkingError.badURLResponse(url: url)
+              }
+        return output.data
+    }
+    
+}
