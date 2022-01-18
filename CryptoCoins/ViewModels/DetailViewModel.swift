e5e98d8947dc78a217935e23734040f16cca4318
Artem Paliutin <pal.artem@icloud.com>
2022-01-18 19:16:49 +0300

    added DetailViewModel

--- CryptoCoins/ViewModels/DetailViewModel.swift
+++ CryptoCoins/ViewModels/DetailViewModel.swift
@@ -11,13 +11,15 @@ import Combine
 
 class DetailViewModel: ObservableObject {
     
-    @Published var showPortfolioView = false
-    @Published var selectedCoin: Coin? = nil
+    @Published var overviewsStatistics: [Statistic] = []
+    @Published var additionalStatistics: [Statistic] = []
     
+    @Published var coin: Coin
     private let coinDetailsService: CoinDetailDataService
     private var cancellable = Set<AnyCancellable>()
     
     init(coin: Coin) {
+        self.coin = coin
         self.coinDetailsService = CoinDetailDataService(coin: coin)
         self.addSubscribers()
     }
@@ -29,10 +31,62 @@ class DetailViewModel: ObservableObject {
     // MARK: - addSubscribers
     private func addSubscribers() {
         coinDetailsService.$coinDetails
-            .sink { returnedCoinDetails in
-                print("Recieved coin detail data")
-                print(returnedCoinDetails)
+            .combineLatest($coin)
+            .map(mapDataToStatistics)
+            .sink { [weak self] returnedArray in
+                self?.overviewsStatistics = returnedArray.overview
+                self?.additionalStatistics = returnedArray.additional
             }
             .store(in: &cancellable)
     }
+    
+    // MARK: - mapDataToStatistics
+    private func mapDataToStatistics(coinDetail: CoinDetail?, coin: Coin) -> (overview: [Statistic], additional: [Statistic]) {
+        // overview
+        let price = coin.currentPrice.asCurrencyWith6Decimals()
+        let pricePercentChange = coin.priceChangePercentage24H
+        let priceStat = Statistic(title: "Current Price", value: price, percentageChange: pricePercentChange)
+        
+        let marketCap = "$" + (coin.marketCap?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentChange = coin.marketCapChangePercentage24H
+        let marketCapStat = Statistic(title: "Market Capitalization", value: marketCap, percentageChange: marketCapPercentChange)
+        
+        let rank = "\(coin.rank)"
+        let rankStat = Statistic(title: "Rank", value: rank)
+        
+        let volume = "$" + (coin.totalVolume?.formattedWithAbbreviations() ?? "")
+        let volumeStat = Statistic(title: "Volume", value: volume)
+        
+        let overviewArray: [Statistic] = [priceStat, marketCapStat, rankStat, volumeStat]
+        
+        
+        // additional
+        let high = coin.high24H?.asCurrencyWith6Decimals() ?? "n/a"
+        let highStat = Statistic(title: "24h High", value: high)
+        
+        let low = coin.low24H?.asCurrencyWith6Decimals() ?? "n/a"
+        let lowStat = Statistic(title: "24h Low", value: low)
+        
+        let priceChange = coin.priceChange24H?.asCurrencyWith2Decimals() ?? "n/a"
+        let pricePercentChange2 = coin.priceChangePercentage24H
+        let priceChangeStat = Statistic(title: "24h Price Change", value: priceChange, percentageChange: pricePercentChange2)
+        
+        let marketCapChange = "$" + (coin.marketCapChangePercentage24H?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentChange2 = coin.marketCapChangePercentage24H
+        let marketCapChangeStat = Statistic(title: "24h Market Cap Chang", value: marketCapChange, percentageChange: marketCapPercentChange2)
+        
+        let blockTime = coinDetail?.blockTimeInMinutes ?? 0
+        let blockTimeString = blockTime == 0 ? "n/a" : "\(blockTime)"
+        let blockStat = Statistic(title: "Block Time", value: blockTimeString)
+        
+        let hashing = coinDetail?.hashingAlgorithm ?? "n/a"
+        let hashingStat = Statistic(title: "Hashing Algorithm", value: hashing)
+        
+        let additionalArray: [Statistic] = [highStat, lowStat, priceChangeStat, marketCapChangeStat, blockStat, hashingStat]
+        
+        
+        return (overviewArray, additionalArray)
+    }
+    
+    
 }
--- CryptoCoins/Views/Detail/DetailView.swift
+++ CryptoCoins/Views/Detail/DetailView.swift
@@ -11,14 +11,53 @@ import SwiftUI
 // MARK: - DetailView
 struct DetailView: View {
     @StateObject var detailVM: DetailViewModel
+    private let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible())]
+    private let spacing: CGFloat = 30
     
     init(coin: Coin) {
         _detailVM = StateObject(wrappedValue: DetailViewModel(coin: coin))
-        print("Init \(coin.name)")
     }
     
     var body: some View {
-        Text("coin.name")
+        ScrollView {
+            VStack(spacing: 20.0) {
+                // MARK: - 
+                Text("")
+                    .frame(height: 150)
+                
+                // MARK: - Title
+                OverviewSubview()
+                Divider()
+                
+                // MARK: - Overview Grid
+                LazyVGrid(
+                    columns: columns,
+                    alignment: .leading,
+                    spacing: spacing,
+                    pinnedViews: []) {
+                        ForEach(detailVM.overviewsStatistics) { stat in
+                            StatisticView(statistic: stat)
+                        }
+                    }
+                
+                // MARK: - Title
+                AdditionalDetailsSubview()
+                Divider()
+                
+                // MARK: - Additional Details Grid
+                LazyVGrid(
+                    columns: columns,
+                    alignment: .leading,
+                    spacing: spacing,
+                    pinnedViews: []) {
+                        ForEach(detailVM.additionalStatistics) { stat in
+                            StatisticView(statistic: stat)
+                        }
+                    }
+            }
+            .padding()
+        }
+        .navigationTitle(detailVM.coin.name)
     }
 }
 
@@ -30,8 +69,35 @@ struct DetailView: View {
 
 
 
+
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: dev.coin)
+        NavigationView {
+            DetailView(coin: dev.coin)
+        }
+    }
+}
+
+
+// MARK: - Subview
+// OverviewSubview
+struct OverviewSubview: View {
+    var body: some View {
+        Text("Overview")
+            .font(.title)
+            .bold()
+            .foregroundColor(.accentColor)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+// AdditionalDetailsSubview
+struct AdditionalDetailsSubview: View {
+    var body: some View {
+        Text("Additional Details")
+            .font(.title)
+            .bold()
+            .foregroundColor(.accentColor)
+            .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
